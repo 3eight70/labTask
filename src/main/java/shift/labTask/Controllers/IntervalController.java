ea6526dd22f8d67275ee36e86b94eb70fff890fe
Vasilev Danil <111416100+3eight70@users.noreply.github.com>
2023-09-24 18:54:00 +0700

    Fix mistakes in merge algorithm

--- src/main/java/shift/labTask/Controllers/IntervalController.java
+++ src/main/java/shift/labTask/Controllers/IntervalController.java
@@ -39,7 +39,7 @@ public class IntervalController {
     public ResponseEntity<Void> addIntervals(@RequestParam("kind") String kind, @RequestBody List<ourInterval> intervals) {
 
         if (kind.equals("digits") || kind.equals("letters")) {
-            List<ourInterval> mergedIntervals = mergeIntervals(intervals);
+            List<ourInterval> mergedIntervals = mergeIntervals(intervals, kind);
 
             if (mergedIntervals != null) {
                 repository.saveAll(mergedIntervals);
@@ -56,30 +56,67 @@ public class IntervalController {
             return second;
         }
     }
-    private List<ourInterval> mergeIntervals(List<ourInterval> intervals){
-        if (intervals.isEmpty()){
+
+    private String compareDigits(String first, String second){
+        if (Integer.parseInt(first) > Integer.parseInt(second)){
+            return first;
+        }
+        else{
+            return second;
+        }
+    }
+    private List<ourInterval> mergeIntervals(List<ourInterval> intervals, String kind) {
+        List<ourInterval> mergedIntervals = new ArrayList<>();
+
+        if (intervals.isEmpty()) {
             return null;
         }
 
-        List<ourInterval> mergedIntervals = new ArrayList<>();
+        if (kind.equals("letters")) {
 
-        Collections.sort(intervals, Comparator.comparing(ourInterval::getFirstInterval));
+            Collections.sort(intervals, Comparator.comparing(ourInterval::getFirstInterval));
 
-        ourInterval current = intervals.get(0);
+            ourInterval current = intervals.get(0);
 
-        for (int i = 1; i<intervals.size(); i++){
-            ourInterval next = intervals.get(i);
+            for (int i = 1; i < intervals.size(); i++) {
+                ourInterval next = intervals.get(i);
 
-            if (current.getLastInterval().charAt(0) >= next.getFirstInterval().charAt(0)){
-                current.setLastInterval(compareLetters(current.getLastInterval(), next.getLastInterval()));
+                if (current.getLastInterval().charAt(0) >= next.getFirstInterval().charAt(0)) {
+                    current.setLastInterval(compareLetters(current.getLastInterval(), next.getLastInterval()));
+                } else {
+                    mergedIntervals.add(current);
+                    current = next;
+                }
             }
-            else{
-                mergedIntervals.add(current);
-                current = next;
-            }
+
+            mergedIntervals.add(current);
+
         }
 
-        mergedIntervals.add(current);
+        else if (kind.equals("digits")) {
+
+            Comparator<ourInterval> intervalComparator = Comparator.comparing(ourInterval::getFirstInterval, (firstNumber, secondNumber) -> {
+                return Integer.compare(Integer.parseInt(firstNumber), Integer.parseInt(secondNumber));
+            });
+
+            Collections.sort(intervals, intervalComparator);
+
+            ourInterval current = intervals.get(0);
+
+            for (int i = 1; i < intervals.size(); i++) {
+                ourInterval next = intervals.get(i);
+
+                if (Integer.parseInt(current.getLastInterval()) >= Integer.parseInt(next.getFirstInterval())) {
+                    current.setLastInterval(compareDigits(current.getLastInterval(), next.getLastInterval()));
+                } else {
+                    mergedIntervals.add(current);
+                    current = next;
+                }
+            }
+
+            mergedIntervals.add(current);
+
+        }
 
         return mergedIntervals;
     }
